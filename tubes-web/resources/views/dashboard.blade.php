bfc09b9f3b600f246daf0d3e0e44c0d5bc685d9a
Adrian Pratama Sasmita <adriansasmita2@gmail.com>
2024-06-04 20:02:15 +0700

    Update WEB

--- tubes-web/resources/views/dashboard.blade.php
+++ tubes-web/resources/views/dashboard.blade.php
@@ -9,28 +9,67 @@
                     <div class="owl-carousel testimonial-carousel">
                         <div class="testimonial-item text-center">
                             <img class="img-fluid rounded-circle mx-auto mb-4" src="img/adil.png" style="width: 200px; height: 200px;">
-                            <h5 class="mb-1">Fadillah Akbar</h5>
-                            <p class="mb-1">Project Manager</p>
-                            <p class="mb-1">Programmer</p>
-                            <p class="mb-1">Analis</p>
+                            <h5 class="mb-1">Fadilah Akbar</h5>
+                            <a href="https://fadilahakbar53.github.io/">Selengkapnya tentang saya</a>
                         </div>
                         <div class="testimonial-item text-center">
                             <img class="img-fluid rounded-circle mx-auto mb-4" src="img/adrian.png" style="width: 200px; height: 200px;">
                             <h5 class="mb-1">Adrian Pratama Sasmita</h5>
-                            <p class="mb-1">Programmer</p>
-                            <p class="mb-1">Tester</p>
+                            <a href="https://adriannprtm.github.io/">Selengkapnya tentang saya</a>
                         </div>
                         <div class="testimonial-item text-center">
                             <img class="img-fluid rounded-circle mx-auto mb-4" src="img/devi.png" style="width: 200px; height: 200px;">
-                            <h5 class="mb-1">Devi</h5>
-                            <p class="mb-1">Designer</p>
+                            <h5 class="mb-1">Devi Febrianti</h5>
+                            <a href="https://piajeu.github.io/">Selengkapnya tentang saya</a>
                         </div>
                         <div class="testimonial-item text-center">
                             <img class="img-fluid rounded-circle mx-auto mb-4" src="img/hasbi.png" style="width: 200px; height: 200px;">
-                            <h5 class="mb-1">Hasbi</h5>
-                            <p class="mb-1">Visualisasi Power-BI</p>
+                            <h5 class="mb-1">Muhammad Hasbi Asshidiqi</h5>
+                            <a href="https://hasbidq.github.io/hasbidiqii.github.io/">Selengkapnya tentang saya</a>
                         </div>
                      </div>
+                </div>
+                <div class="bg-secondary rounded-top h-100 p-4 mt-4">
+                    <h2 class="mb-4 text-center">Kontribusi Anggota Kelompok</h2>
+                </div>
+                <div class="bg-secondary rounded-top h-100 p-4 mt-4">
+                    <h4 class="text-center">Fadilah Akbar</h4>
+                    <p class="mb-1 text-center">Leader</p>
+                    <p class="mb-1 text-center">Project Manager</p>
+                    <p class="mb-1 text-center">Programmer</p>
+                    <p class="mb-1 text-center">Analis</p>
+                    <p class="mb-1 text-center">Documentation</p>
+                </div>
+                <div class="row">
+                    <div class="col ">
+                        <div class="mt-4 card bg-secondary">
+                            <div class="card-body">
+                                <h4>Adrian Pratama Sasmita</h4>
+                                <p class="mb-1">Programmer Web</p>
+                                <p class="mb-1">Tester</p>
+                                <p class="mb-1">Documentation</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="mt-4 card bg-secondary">
+                            <div class="card-body">
+                                <h4>Devi Febrianti</h4>
+                                <p class="mb-1">Designer</p>
+                                <p class="mb-1">Documentation</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="mt-4 card bg-secondary">
+                            <div class="card-body">
+                                <h4>Muhammad Hasbi Asshidiqi</h4>
+                                <p class="mb-1">Visualization PowerBI</p>
+                                <p class="mb-1">Analyst</p>
+                                <p class="mb-1">Documentation</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 @endsection
